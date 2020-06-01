25a0925d917d8df222082ddea268df9a643c7a8f
Anindita Guha <an711755@dal.ca>
2020-06-01 02:10:33 -0300

    Old DB file deleted

---
+++ src/main/java/com/csci5308/grouptool/Database/DBConfigure.java
@@ -0,0 +1,30 @@
+package com.csci5308.grouptool.Database;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBConfigure {
+    public static final String driver="com.mysql.cj.jdbc.Driver";
+    public static final String url="jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_7_DEVINT";
+    public static final String user="CSCI5308_7_DEVINT_USER";
+    public static final String pass="CSCI5308_7_DEVINT_7061";
+
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+}
