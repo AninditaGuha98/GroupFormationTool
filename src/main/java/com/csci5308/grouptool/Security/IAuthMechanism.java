5746d4f5d7550594ee6a0b0d1cdd91e041b7e02a
Raouf Rokhjavan <raouf.rokhjavan@dal.ca>
2020-06-03 21:46:48 -0300

    Add UserAuth to handle email and password Authentication
    - Add unit tests and mock objects
    - Add IAuthMechanism interface

---
+++ src/main/java/com/csci5308/grouptool/Security/IAuthMechanism.java
@@ -0,0 +1,11 @@
+package com.csci5308.grouptool.Security;
+
+import java.util.List;
+
+public interface IAuthMechanism {
+	
+	public boolean isAvailableUser(String userEmail);
+	public boolean isValidUser(String userEmail, String password);
+	public List<String> getUserRoles(String userEmail, String password);
+	
+}
