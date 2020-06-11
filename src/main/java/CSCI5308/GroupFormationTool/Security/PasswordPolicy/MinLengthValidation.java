05b64a8776922b0acc45f33e27078f070c55ccc6
Raouf Rokhjavan <raouf.rokhjavan@dal.ca>
2020-06-11 20:35:33 -0300

    Add Minimum Length Password Policy

--- src/main/java/CSCI5308/GroupFormationTool/Security/PasswordPolicy/MinLengthValidation.java
+++ src/main/java/CSCI5308/GroupFormationTool/Security/PasswordPolicy/MinLengthValidation.java
@@ -5,21 +5,34 @@ public class MinLengthValidation implements IPasswordValidation {
 	private int minLength;
 	
 	public MinLengthValidation() {
+		this.minLength = 8;
 	}
 	
 	public MinLengthValidation(int minLength) {
+		this.setMinLength(minLength);
 	}
 	
-	public int getMinLength( ) {
-		return -1;
+	public int getMinLength() {
+		return this.minLength;
 	}
 	
 	public void setMinLength(int minLength) {
+		if ( minLength <= 0 )
+			this.minLength = 8;
+		else
+			this.minLength = minLength;
 	}
 	
 	@Override
 	public boolean isValidPassword(String password) {
 		// TODO Auto-generated method stub
-		return false;
+		if (null == password)
+			return false;
+		else if (password.isEmpty())
+			return false;
+		if (password.length() >= this.minLength)
+			return true;
+		else 
+			return false;
 	}
 }
