fe5a44a8ef30eeb92273f7475d80e9acee129d77
Anindita Guha <an711755@dal.ca>
2020-06-11 23:45:10 -0300

    Milestone-2 Basecode

---
+++ src/main/java/CSCI5308/GroupFormationTool/CourseAdmin/Interface/IStudentCSVParser.java
@@ -0,0 +1,12 @@
+package CSCI5308.GroupFormationTool.CourseAdmin.Interface;
+
+import java.util.List;
+
+import CSCI5308.GroupFormationTool.AccessControl.User;
+
+public interface IStudentCSVParser 
+{
+
+	public List<User> parseCSVFile(List<String> failureResults);
+	
+}
