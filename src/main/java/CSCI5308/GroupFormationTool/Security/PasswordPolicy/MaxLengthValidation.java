0ad6113015ac6c1088cf57df59294c3330f12422
Raouf Rokhjavan <raouf.rokhjavan@dal.ca>
2020-06-11 22:41:48 -0300

    Add Maximum Length Password Policy

--- src/main/java/CSCI5308/GroupFormationTool/Security/PasswordPolicy/MaxLengthValidation.java
+++ src/main/java/CSCI5308/GroupFormationTool/Security/PasswordPolicy/MaxLengthValidation.java
@@ -2,39 +2,37 @@ package CSCI5308.GroupFormationTool.Security.PasswordPolicy;
 
 public class MaxLengthValidation implements IPasswordValidation {
 
-//	private int maxLength;
+	private int maxLength;
 	
 	public MaxLengthValidation() {
-//		this.maxLength = 12;
+		this.maxLength = 12;
 	}
 	
 	public MaxLengthValidation(int maxLength) {
-//		this.setMaxLength(maxLength);
+		this.setMaxLength(maxLength);
 	}
 	
 	public int getMaxLength() {
-//		return this.maxLength;
-		return -1;
+		return this.maxLength;
 	}
 	
 	public void setMaxLength(int maxLength) {
-//		if (maxLength <= 0)
-//			this.maxLength = 12;
-//		else
-//			this.maxLength = maxLength;
+		if (maxLength <= 0)
+			this.maxLength = 12;
+		else
+			this.maxLength = maxLength;
 	}
 	
 	@Override
 	public boolean isValidPassword(String password) {
-//		if (null == password)
-//			return false;
-//		else if (password.isEmpty())
-//			return false;
-//		
-//		if (password.length() > this.maxLength)
-//			return false;
-//		else
-//			return true;
-		return false;
+		if (null == password)
+			return false;
+		else if (password.isEmpty())
+			return false;
+		
+		if (password.length() > this.maxLength)
+			return false;
+		else
+			return true;
 	}
 }
